7827ff45192283384103d1c55fdf5ef23b13cf89
Jaimitra <141642600+Jaimitra@users.noreply.github.com>
2024-10-31 10:23:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let button=document.getElementById("but");
+button.addEventListener("click",(event)=>{
+	let fn =document.getElementById("fn");
+	let ln=document.getElementById("ln");
+	let pn=document.getElementById("pn");
+	let ed=document.getElementById("ed")
+	alert("First Name "+fn.value" Last Name "+ln.value" Phone Number: "+pn.value" Email ID: "+ed.value);
+})
